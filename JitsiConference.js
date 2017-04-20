3e8d566036985dcb5d42eb1af66121f7a7177765
paweldomas <pawel.domas@jitsi.org>
2017-04-20 16:41:25 -0500

    fix(JitsiConference): a crash
    Avoid a crash if for whatever reason there is no JVB JingleSessionPC at
    the time when P2P is being stopped.
    
    Add FIXME about possible situation where Jicofo invite will arrive after
    the P2P has been established already.

--- JitsiConference.js
+++ JitsiConference.js
@@ -2019,6 +2019,9 @@ JitsiConference.prototype._onIceConnectionEstablished
 
     // Stop media transfer over the JVB connection
     if (this.jvbJingleSession) {
+        // FIXME if for whatever reason invite from Jicofo for the JVB
+        // connection arrives, after the P2P has been established
+        // this needs to be called as well.
         this._suspendMediaTransferForJvbConnection();
     }
 
@@ -2291,7 +2294,9 @@ JitsiConference.prototype._stopP2PSession
 
     // Swap remote tracks, but only if the P2P has been fully established
     if (wasP2PEstablished) {
-        this._resumeMediaTransferForJvbConnection();
+        if (this.jvbJingleSession) {
+            this._resumeMediaTransferForJvbConnection();
+        }
 
         // Remove remote P2P tracks
         this._removeRemoteP2PTracks();
